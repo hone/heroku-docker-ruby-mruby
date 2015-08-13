d57789db4b44389cadf1a89995bdd73fb50fb541
Terence Lee <hone02@gmail.com>
2015-08-12 21:39:43 -0400

    clean up after test

--- test/test_install_ruby.rb
+++ test/test_install_ruby.rb
@@ -13,6 +13,8 @@ GEMFILE
       assert_include File.read(profiled_path), %Q{export PATH="#{install_path}/ruby-2.2.2/bin:$PATH"}
       assert_include File.read(profiled_path), %Q{export GEM_PATH="#{install_path}/bundle/ruby/2.2.0"}
       assert_include File.read(profiled_path), %Q{export GEM_HOME="#{install_path}/bundle/ruby/2.2.0"}
+    ensure
+      IO.popen("rm -rf #{install_path}")
     end
 
     def test_run_install_path_extra_trailing_slash
@@ -24,6 +26,8 @@ ruby "2.2.2"
 GEMFILE
       assert_include output.string, "ruby-2.2.2"
       assert Dir.exist?("#{install_path}/ruby-2.2.2")
+    ensure
+      IO.popen("rm -rf #{install_path}")
     end
   end
 end

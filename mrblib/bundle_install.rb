d48be36dd31009f2ebce2a260c99c0454d98dadb
Terence Lee <hone02@gmail.com>
2015-08-20 07:46:07 -0400

    don't skip dev/test gems, this is meant for development

--- mrblib/bundle_install.rb
+++ mrblib/bundle_install.rb
@@ -5,7 +5,7 @@ module CLI
     end
 
     def run(vendor_path, profiled_path)
-      command = "bundle install --path #{vendor_path} --deployment --without development:test --jobs 4"
+      command = "bundle install --path #{vendor_path} --jobs 4"
       Util.pipe(command, @output_io, profiled_path)
     end
   end

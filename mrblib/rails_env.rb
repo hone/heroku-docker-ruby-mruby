5f9648caf2aa4dce0fb7787b807892fd603343aa
Terence Lee <hone02@gmail.com>
2015-08-31 17:23:47 -0400

    only setup rails env for a rails app

--- mrblib/rails_env.rb
+++ mrblib/rails_env.rb
@@ -10,10 +10,14 @@ module CLI
     private
     def write_profiled(profiled_path)
       Util.mkdir_p(File.dirname(profiled_path))
+      gem_detector = DetectGem.new(profiled_path)
+      profiled     = Profiled.new
 
-      profiled = Profiled.new
-      profiled.default("RAILS_ENV", "production")
-      profiled.default("SECRET_KEY_BASE", generate_key)
+      if railties_version = gem_detector.gem("railties")
+        railties_version = Gem::Version.new(railties_version)
+        profiled.default("RAILS_ENV", "production")
+        profiled.default("SECRET_KEY_BASE", generate_key) if railties_version >= Gem::Version.new("4.1.0.beta1")
+      end
 
       File.open(profiled_path, 'a') {|file| file.puts profiled.string }
     end

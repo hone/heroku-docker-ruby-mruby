0213d1752d75169c8c63da5dc4d53c4e6bd0a1ac
Terence Lee <hone02@gmail.com>
2015-09-02 10:09:09 -0400

    fix intall ruby mtests

--- test/test_install_ruby.rb
+++ test/test_install_ruby.rb
@@ -19,7 +19,7 @@ GEMFILE
 
     def test_run_install_path_extra_trailing_slash
       output       = StringIO.new
-      install_path = "/tmp/ruby-#{rand(Time.now.usec).to_s(36)}/"
+      install_path = "/tmp/ruby-#{rand(Time.now.usec).to_s(36)}"
       InstallRuby.new(output).run(<<GEMFILE, install_path)
 ruby "2.2.2"
 
@@ -32,8 +32,9 @@ GEMFILE
 
     def test_default_ruby
       output       = StringIO.new
-      install_path = "/tmp/ruby-#{rand(Time.now.usec).to_s(36)}/"
-      InstallRuby.new(output).run(<<GEMFILE, install_path)
+      install_path = "/tmp/ruby-#{rand(Time.now.usec).to_s(36)}"
+      profiled_path = "#{install_path}/.profile.d/ruby.sh"
+      InstallRuby.new(output).run(<<GEMFILE, install_path, profiled_path)
 GEMFILE
       assert_include output.string, "ruby-2.2.2"
       assert Dir.exist?("#{install_path}/ruby-2.2.2")
